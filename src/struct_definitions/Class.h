5b7e74932f5c7e76f7cd8fb55007afd334b07ed9
hnhatnam <namzt1975@gmail.com>
2023-03-25 10:03:47 +0700

    Edit Struct

--- src/struct_definitions/Class.h
+++ src/struct_definitions/Class.h
@@ -5,5 +5,7 @@ struct Class
     std::string name;
     std::string code;
     std::string enter_year;
+    unsigned int max_students     = 50;
+    unsigned int current_students = 0;
     LLNode *student_list;
 };
