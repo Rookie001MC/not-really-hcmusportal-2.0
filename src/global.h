c7075e007b356d3852cf2072b2a0b43442e088f1
Rookie Nguyễn <nguyenquocthang2004@gmail.com>
2023-03-27 13:33:24 +0700

    fuck myself man no more stupid `#includes`

--- src/global.h
+++ src/global.h
@@ -1,13 +1,5 @@
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <string>
-#include "struct_definitions/Class.h"
-#include "struct_definitions/Course.h"
-#include "struct_definitions/Enums.h"
-#include "struct_definitions/LinkedListNode.h"
-#include "struct_definitions/User.h"
 
-struct SchoolYear
-{
-    std::string school_year;
-    LLNode *class_list;
-};
