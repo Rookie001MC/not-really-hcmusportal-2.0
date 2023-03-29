522f29f17dd9803e13f13970ebf0ce3b96c39591
taokobik1234 <thienvovinpro123@gmail.com>
2023-03-29 16:20:56 +0700

    merge struct

--- src/struct_definitions/struct.h
+++ src/struct_definitions/struct.h
@@ -1,1 +1,62 @@
-#pragma once
+﻿
+#include <string>
+
+
+struct Date
+{
+    unsigned int day;
+    unsigned int month;
+    unsigned int year;
+};
+
+struct Class
+{
+    unsigned int id;
+    std::string name;
+    std::string code;
+    std::string enter_year;
+    unsigned int max_students     = 50;
+    unsigned int current_students = 0;
+    Class *classNext              = nullptr;
+    Student *studentList          = nullptr;
+};
+struct Student
+{
+    unsigned int id;
+    std::string name;
+    Date dob;
+    Gender gender;
+    unsigned int social_id;
+    Student *studentNext = nullptr;
+};
+struct Course
+{
+    unsigned int id;
+    std::string name;
+    std::string code;
+    std::string teacher_name;
+    unsigned int credits;
+    unsigned int max_students     = 50;
+    unsigned int current_students = 0;
+    // A course can only have so much students, so it's better to use an array here instead
+    // Dizzme mùa đăng ký học phần
+    Student *student_list;
+};
+
+enum Gender
+{
+    MALE    = 0,
+    FEMALE  = 1,
+    NOT_SAY = 2
+};
+
+enum DayOfWeek
+{
+    MONDAY    = 0,
+    TUESDAY   = 1,
+    WEDNESDAY = 2,
+    THURSDAY  = 3,
+    FRIDAY    = 4,
+    SATURDAY  = 5,
+    SUNDAY    = 6
+};
