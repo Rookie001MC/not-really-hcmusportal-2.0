22ed51a4ffe09173431ba88ed3c055082356244c
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-04-02 13:23:19 +0700

    Fixing directories of db, now it's DIY copy game

--- src/functions/Login/Login.h
+++ src/functions/Login/Login.h
@@ -6,7 +6,7 @@
 #include "../FileIO/FileIO.h"
 
 const std::string loginDataFile      = "login.csv";
-const std::string loginDataDirectory = "../../db/";
+const std::string loginDataDirectory = "./db/";
 
 std::string login();
 bool findUserInCSVList(CSVList *list, std::string username, std::string password);
