e6d3868d17af518b9af0cbc53ced552f6f8cc929
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-04-02 17:06:45 +0700

    Sketching layout of file

---
+++ src/functions/ChangePassword/ChangePassword.cpp
@@ -0,0 +1,40 @@
+#include "ChangePassword.h"
+
+void changePassword(std::string username)
+{
+    RowResult userRow = SearchSingleCSVRecord(loginDataFile, dataDirectory, username);
+    if (userRow.errorMsg != "")
+    {
+        std::cout << userRow.errorMsg << std::endl;
+        return;
+    }
+
+    std::string oldPassword;
+    std::string newPassword;
+    std::string retypePassword;
+
+    do {
+        std::cout << "Enter your old password: ";
+        std::cin >> oldPassword;
+        if (oldPassword.compare(userRow.row->columns[1]))
+        {
+            std::cout << "Wrong password!" << std::endl;
+        }
+    } while (oldPassword.compare(userRow.row->columns[1]));
+    
+
+    do {
+        std::cout << "Enter your new password: ";
+        std::cin >> newPassword;
+        std::cout << "Retype your new password: ";
+        std::cin >> retypePassword;
+        if (newPassword.compare(retypePassword))
+        {
+            std::cout << "Password does not match!" << std::endl;
+        }
+    } while (newPassword.compare(retypePassword));
+    
+
+    userRow.row->columns[1] = newPassword;
+    
+}
---
+++ src/functions/ChangePassword/ChangePassword.h
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <string>
+#include "../../constants.h"
+#include "../FileIO/FileIO.h"
