6299d12006b064405e24d3db9d71fa63f655e8b2
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-04-02 14:34:05 +0700

    Set all includes here instead

--- src/global.h
+++ src/global.h
@@ -1,4 +1,12 @@
+#pragma once
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <string>
+
+#include "functions/FileIO/FileIO.h"
+#include "functions/Login/Login.h"
+#include "functions/Menu/Menu.h"
+#include "functions/Session/Session.h"
+
+void runLogic();
