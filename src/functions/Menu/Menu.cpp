dc8084de99703a096c26fc34d1577ef8de03e434
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-04-02 16:26:44 +0700

    Organized menus a little bit

--- src/functions/Menu/Menu.cpp
+++ src/functions/Menu/Menu.cpp
@@ -6,7 +6,7 @@
 const std::string profileFile      = "profile.csv";
 const std::string profileDirectory = "../../db";
 
-void Menu(std::string username)
+void MainMenu(std::string &username)
 {
     int choice;
 
@@ -17,14 +17,14 @@ void Menu(std::string username)
     {
         do
         {
-            std::cout << "1. View Profile" << std::endl
+            std::cout << "1. Account settings" << std::endl
                       << "2. Class Management" << std::endl
                       << "3. Course Management" << std::endl
                       << "Enter your choice : ";
             std::cin >> choice;
             if (choice == 1)
             {
-                PrintProfile(currentUserProfile);
+                AccountMenu(username, currentUserProfile);
             }
             else if (choice == 2)
             {}
@@ -40,14 +40,14 @@ void Menu(std::string username)
     {
         do
         {
-            std::cout << "1. View Profile" << std::endl
+            std::cout << "1. Account settings" << std::endl
                       << "2. View course" << std::endl
                       << "3. Scoreboard" << std::endl
                       << "Enter your choice : ";
             std::cin >> choice;
             if (choice == 1)
             {
-                PrintProfile(currentUserProfile);
+                AccountMenu(username, currentUserProfile);
             }
             else if (choice == 2)
             {}
@@ -59,4 +59,34 @@ void Menu(std::string username)
             clear_screen();
         } while (choice != 1 && choice != 2 && choice != 3);
     }
+}
+
+void AccountMenu(std::string &username, User &currentUserProfile)
+{
+    int choice;
+    do
+    {
+        std::cout << "1. View Profile" << std::endl
+                  << "2. Change Password" << std::endl
+                  << "3. Logout" << std::endl
+                  << "Enter your choice: ";
+        std::cin >> choice;
+        if (choice == 1)
+        {
+            PrintProfile(currentUserProfile);    
+        }
+        else if (choice == 2)
+        {
+        }
+        else if (choice == 3)
+        {
+            
+        }
+        else
+        {
+            std::cout << "Invalid input";
+        }
+        wait_for_enter();
+        clear_screen();
+    } while (choice != 1 && choice != 2);
 }
--- src/functions/Menu/Menu.h
+++ src/functions/Menu/Menu.h
@@ -1,4 +1,7 @@
 #pragma once
 #include <iostream>
+#include "../../struct_definitions/User.h"
 #include "../FileIO/FileIO.h"
-void Menu(std::string username);
+
+void MainMenu(std::string &username);
+void AccountMenu(std::string &username, User &currentUserProfile);
