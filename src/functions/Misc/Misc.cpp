6c48973d47bcde17f2c058cec8f2ad26aa7c50f9
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-04-04 01:33:01 +0700

    dizzme windows \n rồi sao lại \r

--- src/functions/Misc/Misc.cpp
+++ src/functions/Misc/Misc.cpp
@@ -11,3 +11,18 @@ void clear_screen()
 {
     std::cout << "\033[2J\033[1;1H";
 }
+
+void removeHiddenNewlineChar(std::string &str)
+{
+    // Classic newline
+    if (str[str.length() - 1] == '\n')
+    {
+        str.erase(str.length() - 1);
+    }
+
+    // Windows newline
+    if (str[str.length() - 1] == '\r')
+    {
+        str.erase(str.length() - 1);
+    }
+}
