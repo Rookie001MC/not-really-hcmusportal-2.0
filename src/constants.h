5173bd84443aa94fb87fcf9261d2b8526421714e
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-04-04 10:32:13 +0700

    Bunch of databases declaration

--- src/constants.h
+++ src/constants.h
@@ -1,5 +1,12 @@
 #include <string>
 #pragma once
+
 const std::string loginDataFile   = "login.csv";
 const std::string profileDataFile = "profile.csv";
-const std::string dataDirectory   = "./db/";
+const std::string courseDataFile  = "course.csv";
+const std::string classDataFile   = "class.csv";
+const std::string scoreDataFile   = "score.csv";
+const std::string schoolYearFile  = "schoolyear.csv";
+const std::string semesterFile    = "semester.csv";
+
+const std::string dataDirectory = "./db/";
