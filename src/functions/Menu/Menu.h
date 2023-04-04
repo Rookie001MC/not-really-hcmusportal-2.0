e996533dec7f51154aceefa1641abc72197f4846
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-04-04 10:51:00 +0700

    Separate SchoolYear to its own directory

--- src/functions/Menu/Menu.h
+++ src/functions/Menu/Menu.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <iostream>
+
 #include "../../constants.h"
 #include "../../struct_definitions/User.h"
+
 #include "../ChangePassword/ChangePassword.h"
 #include "../FileIO/FileIO.h"
 #include "../Logout/Logout.h"
 #include "../Misc/Misc.h"
 #include "../Profile/Profile.h"
+#include "../SchoolYear/SchoolYear.h"
 
 void MainMenu(std::string &username);
 
