7d016b4d47e0171cc2c1b8c8973c18d6c88cbd81
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-04-04 10:51:00 +0700

    Separate SchoolYear to its own directory

--- src/functions/Menu/Menu.h
+++ src/functions/Menu/Menu.h
@@ -1,26 +1,18 @@
 #pragma once
 #include <iostream>
+
 #include "../../constants.h"
 #include "../../struct_definitions/User.h"
+
 #include "../ChangePassword/ChangePassword.h"
 #include "../FileIO/FileIO.h"
 #include "../Logout/Logout.h"
 #include "../Misc/Misc.h"
 #include "../Profile/Profile.h"
+#include "../SchoolYear/SchoolYear.h"
 
 void MainMenu(std::string &username);
 
 void AccountMenu(std::string &username, User &currentUserProfile);
-
 void SchoolYearMenu();
-void CreateSchoolYear();
-void DeleteSchoolYear();
-void PromptNewSchoolYear();
-void promptChoice();
-void PromptNoSchoolYear();
-int getNextSchoolYearID();
-
 void ClassManagementMenu();
-
-
-bool checkValidYear(unsigned int year);
