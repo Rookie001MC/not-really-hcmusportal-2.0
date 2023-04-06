9b12e70e3e519e3c1040d033d907949c4c9dbb0c
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-04-06 10:52:17 +0700

    Moved SplitDates to here, added functions and header guards
    Added checkValidDate and getNextIdOfCSV

--- src/functions/Misc/Misc.cpp
+++ src/functions/Misc/Misc.cpp
@@ -26,3 +26,74 @@ void removeHiddenNewlineChar(std::string &str)
         str.erase(str.length() - 1);
     }
 }
+void SplitDates(std::string date, Date &dateObject)
+{
+    std::string temp;
+    int i          = 0;
+    char delimiter = '-';
+    while (date[i] != delimiter)
+    {
+        temp += date[i];
+        i++;
+    }
+    dateObject.year = std::stoi(temp);
+
+    temp = "";
+    i++;
+
+    while (date[i] != delimiter)
+    {
+        temp += date[i];
+        i++;
+    }
+
+    dateObject.month = std::stoi(temp);
+
+    temp = "";
+    i++;
+
+    while (i < date.length())
+    {
+        temp += date[i];
+        i++;
+    }
+
+    dateObject.day = std::stoi(temp);
+}
+bool checkValidDate(Date date)
+{
+    if (date.day < 1 || date.day > 31)
+    {
+        return false;
+    }
+    if (date.month < 1 || date.month > 12)
+    {
+        return false;
+    }
+    if (date.year < 2000 || date.year > 2100)
+    {
+        return false;
+    }
+    return true;
+}
+
+int getNextIdOfCSV(std::string filename, std::string directory)
+{
+    ListResult fileContent = readCSV(filename, directory);
+
+    // No data in the file
+    if (fileContent.list->size == 0)
+    {
+        return 1;
+    }
+
+    // Get the ID of the last row
+    CSVNode *current = fileContent.list->head->next; // Skip header
+
+    for (int i = 0; i < fileContent.list->size - 1; i++)
+    {
+        current = current->next;
+    }
+
+    return std::stoi(current->data->columns[0]) + 1;
+}
